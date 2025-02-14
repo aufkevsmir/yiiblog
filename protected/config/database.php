df3c67f50a25db6485b4cdbb5bee9878cc65c007
aufkevsmir <miranda.kevin@auf.edu.ph>
2025-02-14 14:45:06 +0800

    Initial Commit

--- protected/config/database.php
+++ protected/config/database.php
@@ -4,11 +4,12 @@
 return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+	
+	'connectionString' => 'mysql:host=localhost;dbname=blog_db',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',
 	'charset' => 'utf8',
-	*/
+	'tablePrefix' => 'tbl_',
+	
 );
